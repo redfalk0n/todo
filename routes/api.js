c51039be84beec962e37581400dba5e8c7903d3e
Dima Shesterikov <dima.shishterikov@lincx.la>
2019-06-26 13:34:47 +0300

    telegramm message

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
 
 router.post('/netlify', (req, res, next) => {
   res.send(JSON.stringify(req.body))
+  axios.post(
+      'https://api.telegram.org/bot890368618:AAF-8rh-KMTZg_uX5EKyp9bVKNjJZoyf2Vo/sendMessage',
+      {
+        text: JSON.stringify(req.body),
+        chat_id: 253527664
+      }
+  ).then(res => {console.log(res)})
+      .catch(err => {console.log(err)})
 })
 
 module.exports = router;
