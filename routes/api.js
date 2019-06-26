2747240feb89e82677b69c677e7acde32cbf7bbc
Dima Shesterikov <dima.shishterikov@lincx.la>
2019-06-26 13:19:23 +0300

    initial commit

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.post('/', function(req, res, next) {
-  console.log(req.body)
-  res.send('respond with a resource');
-});
-
+router.post('/netlify', (req, res, next) => {
+  res.send(JSON.stringify(req.body))
+})
 
 module.exports = router;
