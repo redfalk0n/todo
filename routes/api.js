0f0f3efd5096117e49f5e337af4a89a6f09f05d9
pbespalov <pbespalov@rnds.pro>
2019-10-08 19:18:58 +0300

    fix2

--- routes/api.js
+++ routes/api.js
@@ -24,22 +24,24 @@ router.post('/netlify', (req, res, next) => {
 router.post('/pictureTest', (req, res, next) => {
   if (req.body.message.text === "/raspisaniehochu") {
     axios.get('https://www.site-shot.com/screenshot/?width=1024&height=768&zoom=100&scaled_width=1024&full_size=&format=PNG&user_agent=&rnd=29814755364865&url=http%3A%2F%2Fschedule.npi-tu.ru%2Fschedule%2Ffitu%2F2%2F5m')
-      .then(res => {
-        res.send('Success')
+      .then(resp => {
+        resp.send('Success')
         axios.post(
           botApi + 'sendPhoto',
           {
             chat_id: 253527664,
-            photo: `https://www.site-shot.com/cached_image/${res.data.b64_uuid}`
+            photo: `https://www.site-shot.com/cached_image/${resp.data.b64_uuid}`
           }
-        ).then(res => {
-          console.log(res)
-          res.send('Success')
+        ).then(resp => {
+          console.log(resp)
+          resp.send('Success')
         })
         .catch(err => {
           console.log(err)
-          res.send(err)
+          resp.send(err)
         })
+      }).catch(err => {
+        res.send(err)
       })
   } else {
     axios.post(
