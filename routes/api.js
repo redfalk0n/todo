4cf4db67f0b42290d0aea8c2a28a90b29770f3ff
pbespalov <pbespalov@rnds.pro>
2019-10-08 17:32:32 +0300

    pic test

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,33 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios')
 
+const botApi = 'https://api.telegram.org/bot890368618:AAF-8rh-KMTZg_uX5EKyp9bVKNjJZoyf2Vo/'
+
 router.post('/netlify', (req, res, next) => {
-  res.send(JSON.stringify(req.body))
   axios.post(
-      'https://api.telegram.org/bot890368618:AAF-8rh-KMTZg_uX5EKyp9bVKNjJZoyf2Vo/sendMessage',
+      botApi + 'sendMessage',
       {
         text: `Successful deploy. \n ${req.body.url}`,
         chat_id: 253527664
       }
-  ).then(res => {console.log(res)})
-      .catch(err => {console.log(err)})
+  ).then(res => {
+      console.log(res)
+      res.send('Success')
+    })
+    .catch(err => {
+      console.log(err)
+      res.send(err)
+    })
+})
+
+router.post('/pictureTest', (req, res, next) => {
+  axios.post(
+    botApi + 'sendPhoto',
+    {
+      chat_id: 253527664,
+      photo: 'https://picsum.photos/500'
+    }
+  )
 })
 
 module.exports = router;
